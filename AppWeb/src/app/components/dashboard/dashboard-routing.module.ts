f8be4594bd8b63fc70d6401fda163dcddddcb9de
Esteban Morales <eamu11@hotmail.com>
2022-05-16 16:46:12 -0600

    Frontend

---
+++ AppWeb/src/app/components/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ReportesComponent } from './reportes/reportes.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { ChallengesComponent } from './challenges/challenges.component';
+
+const routes: Routes = [
+    { path: '', component: DashboardComponent, children: [
+      { path: '', component: InicioComponent },
+      { path: 'usuarios', component: UsuariosComponent },
+      { path: 'reportes', component: ReportesComponent },
+      { path: 'challenges', component: ChallengesComponent },
+    ]  
+  },
+    
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
---
+++ AppWeb/src/app/components/dashboard/dashboard.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SharedModule } from '../shared/shared.module';
+import { ReportesComponent } from './reportes/reportes.component';
+import { ChallengesComponent } from './challenges/challenges.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    UsuariosComponent,
+    InicioComponent,
+    NavbarComponent,
+    ReportesComponent,
+    ChallengesComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    SharedModule
+  ]
+})
+export class DashboardModule { }
