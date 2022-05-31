3c10deb3d96c416f5aaff7d0dfd3a8afa252eef9
Anthony <anthonymoro08@gmail.com>
2022-05-31 05:33:18 +0000

    [fix] verificación del login
    Se agrega función que verifica que el usuario y contraseña coincidan con los deportistas registrados en la aplicación

--- AppWeb/src/app/components/login/login.component.ts
+++ AppWeb/src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/Models/usuario';
+import { UsuarioService } from 'src/app/Services/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -11,8 +13,10 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   logo = 'http://assets.stickpng.com/images/5847ea22cef1014c0b5e4833.png'
   form: FormGroup
+  usersList:Usuario[] = []
+  logged:Boolean = false
   
-  constructor(private router: Router, private fb: FormBuilder, private _snackBar: MatSnackBar) { 
+  constructor(private router: Router, private fb: FormBuilder, private _snackBar: MatSnackBar, private userService:UsuarioService) { 
     this.form = this.fb.group({
       usuario: ['', Validators.required],
       password: ['', Validators.required]
@@ -20,18 +24,26 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe((data:any)=>{this.usersList = data})
   }
 
-  ingresar() {
+  goToPage(pageName:string){
+    this.router.navigate([`${pageName}`]);
+  }
+
+  checkLoginDeportistas(){
     const usuario = this.form.value.usuario;
     const password = this.form.value.password;
-    if(usuario == 'SA' && password == 'sa'){
-      this.router.navigateByUrl('/dashboard');
-    }else{
+    for (var i=0; i<this.usersList.length; i++){
+      if (usuario == this.usersList[i].usuario && password == this.usersList[i]._password && this.usersList[i].id_rol == 2) {
+        this.goToPage('/dashboard')
+        this.logged = true
+      }
+    }
+    if (!this.logged){
       this.error()
     }
   }
-
 
   ingresarAdmin() {
     const usuario = this.form.value.usuario;
@@ -41,12 +53,6 @@ export class LoginComponent implements OnInit {
     }else{
       this.error()
     }
-  }
-
-  
-
-  registrar() {
-    this.router.navigateByUrl('/registro');
   }
 
   error() {
