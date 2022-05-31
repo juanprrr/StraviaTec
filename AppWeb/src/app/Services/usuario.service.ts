2664bf1aa7268916276f0dc35cba8127e41029f4
Anthony <anthonymoro08@gmail.com>
2022-05-31 05:04:39 +0000

    [add] registro de usuarios
    Se agrega el registro de deportistas a la base de datos

---
+++ AppWeb/src/app/Services/usuario.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../Models/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  url = '/api/Usuario';
+  constructor(private httpclient:HttpClient) { }
+
+  //GetUsers
+  getUsers():Observable<any>{
+    return this.httpclient.get(this.url)
+  }
+  //GetUserbyId
+  getUserbyId(Id:string):Observable<Usuario>{
+    let params = new HttpParams().set('id', Id)
+    return this.httpclient.get<Usuario>(this.url, {params:params})
+  }
+  //PostUser
+  insertUser(user:Usuario):Observable<any>{
+    return this.httpclient.post(this.url, user, this.generateHeaders())
+  }
+  //UpdateUser
+  updateUser(user:Usuario):Observable<any>{
+    return this.httpclient.put(this.url + "/" + user.usuario, user,  this.generateHeaders())
+  }
+  //DeleteUser
+  deleteUser(Id:number):Observable<any>{
+    return this.httpclient.delete(this.url + "/" +  Id, this.generateHeaders())
+  }
+  private generateHeaders = () => {
+    return {
+      headers: new HttpHeaders({
+        "Access-Control-Allow-Origin": "*", 
+        'Content-Type': 'application/json'
+      })
+    }
+  }
+}
--- AppWeb/src/app/components/registro/registro.component.ts
+++ AppWeb/src/app/components/registro/registro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/Models/usuario';
+import { UsuarioService } from 'src/app/Services/usuario.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  //form: FormGroup
+  newUser:Usuario = new Usuario
 
-  constructor() { }
+  constructor(private userService:UsuarioService, public router: Router) { }
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe((res: any)=>{
+      console.log('Res: ', res)
+    })
   }
 
+  goToPage(pageName:string){
+    this.router.navigate([`${pageName}`]);
+  }
+  
+  addUser(newUse:Usuario){
+    newUse.id_rol = 2
+    newUse.id_carrera = undefined
+    newUse.foto = undefined
+    this.userService.insertUser(newUse).subscribe(()=>{
+      window.location.reload()
+      alert("Registro completado!")
+    },()=>alert("No se pudo registrar su usuario, por favor intente de nuevo!"))
+  }
 }
