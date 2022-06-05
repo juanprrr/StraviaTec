133da42c6e4401f2f8951a80eb22c6ba1032ec08
Anthony <anthonymoro08@gmail.com>
2022-06-05 23:26:59 +0000

    [update] Funcionalidad amigos
    Se crea la funcionalidad para ver a los amigos del usuario loggeado

--- AppWeb/src/app/components/dashboard/training/friends/friends.component.ts
+++ AppWeb/src/app/components/dashboard/training/friends/friends.component.ts
@@ -3,6 +3,8 @@ import { UsuarioService } from 'src/app/Services/usuario.service';
 import * as moment from 'moment';
 import { MatTableDataSource } from '@angular/material/table';
 import { Usuario } from 'src/app/Models/usuario';
+import { UsuarioSigueUsuarioService } from 'src/app/Services/usuario-sigue-usuario.service';
+import { UsuarioSigueUsuario } from 'src/app/Models/usuario-sigue-usuario';
 
 @Component({
   selector: 'app-friends',
@@ -11,20 +13,45 @@ import { Usuario } from 'src/app/Models/usuario';
 })
 export class FriendsComponent implements OnInit {
   displayedColumns: string[] = ['usuario', 'nombre', 'apellidos', 'nacionalidad', 'iconos'];
-  CurrentDate = new Date()
+  currentUser:Usuario = new Usuario
+  CurrentDate = new Date();
   dataSource:any;
 
-  constructor(private userService:UsuarioService) { }
+  constructor(private userService:UsuarioService, private followUserService:UsuarioSigueUsuarioService) { }
 
   ngOnInit(): void {
+    this.loadCurrentUser()
     this.userService.getUsers().subscribe((res: any)=>{
-      this.dataSource = new MatTableDataSource(res);
+      this.getFollowedUsers(res)
+    })
+  }
+
+  getFollowedUsers(allUsers:any) {
+    let listFollowedUsers: Usuario[] = []
+
+    this.followUserService.getUsuarioSigueUsuarios().subscribe((res: any)=>{
+      
+      for (var i=0; i<res.length; i++){
+        if (res[i].id_user == this.currentUser.usuario) {
+          for (var j=0; j<allUsers.length; j++){
+            if (res[i].user_sigue == allUsers[j].usuario){
+              listFollowedUsers.push(allUsers[j])
+            }
+          }
+        }
+      }
+
+      this.dataSource = new MatTableDataSource(listFollowedUsers);
     })
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  loadCurrentUser() {
+    this.currentUser = this.userService.getCurrentUser()
   }
 
   viewUserProfile(friend: Usuario) {
