e7bc764c5b60d0a637ee2abf3b400e9c8bbef8a0
igmochang <ig.morales@estudiantec.cr>
2022-06-06 11:51:08 -0600

    [Update] Deleted useless folders

--- MyApplication/app/src/main/java/ac/cr/tec/myapplication/ui/login/LoggedInUserView.kt
+++
@@ -1,9 +0,0 @@
-package ac.cr.tec.myapplication.ui.login
-
-/**
- * User details post authentication that is exposed to the UI
- */
-data class LoggedInUserView(
-    val displayName: String
-    //... other data fields that may be accessible to the UI
-)
