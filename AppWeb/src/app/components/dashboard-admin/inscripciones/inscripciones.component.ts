37815ece705f7fd5d4daead3ad56f6d0eb887a35
Esteban Morales <eamu11@hotmail.com>
2022-06-06 16:18:03 -0600

    Adjuncion de tabla

--- AppWeb/src/app/components/dashboard-admin/inscripciones/inscripciones.component.ts
+++ AppWeb/src/app/components/dashboard-admin/inscripciones/inscripciones.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface TableElement {
+  Nombre: string;
+  Admin: string;
+  
+}
+
+const ELEMENT_DATA: TableElement[] = [
+  {Nombre: 'Grupo Cartago', Admin: 'Jorge Jimenez'},
+  {Nombre: 'Grupo Alajuelense', Admin: 'Carlos Perez'},
+  {Nombre: 'Grupo Caminantes', Admin: 'Grace Ulloa'},
+];
+
 @Component({
   selector: 'app-inscripciones',
   templateUrl: './inscripciones.component.html',
@@ -7,9 +19,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscripcionesComponent implements OnInit {
 
+  displayedColumns: string[] = ['Nombre', 'Admin', 'acciones'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
