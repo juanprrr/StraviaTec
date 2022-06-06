f57186fd2f2d72522dc7494b9fd0a0ddfb0f219f
Esteban Morales <eamu11@hotmail.com>
2022-06-06 16:08:18 -0600

    soluion errores varios

--- AppWeb/src/app/components/dashboard-admin/gestion-carrera/gestion-carrera.component.ts
+++ AppWeb/src/app/components/dashboard-admin/gestion-carrera/gestion-carrera.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface TableElement {
+  Nombre: string;
+  Fecha: string;
+  Recorrido: string;
+  Tipo: string;
+}
+
+const ELEMENT_DATA: TableElement[] = [
+  {Nombre: 'Reto Irazu', Fecha: '11/11/2022', Recorrido: 'Prusia', Tipo: 'Carrera'},
+  {Nombre: 'Reto Poas', Fecha: '11/11/2022', Recorrido: 'Poas', Tipo: 'Caminata'},
+  {Nombre: 'Aguas abiertas', Fecha: '11/11/2022', Recorrido: 'Herradura', Tipo: 'Natacion'},
+  {Nombre: 'Valle de Orosi', Fecha: '11/11/2022', Recorrido: 'Orosi', Tipo: 'Ciclismo'},
+  {Nombre: 'Jaco Extremo', Fecha: '11/11/2022', Recorrido: 'Jaco', Tipo: 'Reto'},
+];
+
 @Component({
   selector: 'app-gestion-carrera',
   templateUrl: './gestion-carrera.component.html',
   styleUrls: ['./gestion-carrera.component.css']
 })
+
 export class GestionCarreraComponent implements OnInit {
 
+  displayedColumns: string[] = ['Nombre', 'Fecha', 'Recorrido', 'Tipo'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
--- AppWeb/src/app/components/dashboard-admin/reportes-participantes/reportes-participantes.component.ts
+++ AppWeb/src/app/components/dashboard-admin/reportes-participantes/reportes-participantes.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface TableElement {
+  Nombre: string;
+  Fecha: string;
+  Recorrido: string;
+  Tipo: string;
+}
+
+const ELEMENT_DATA: TableElement[] = [
+  {Nombre: 'Reto Irazu', Fecha: '11/11/2022', Recorrido: 'Prusia', Tipo: 'Carrera'},
+  {Nombre: 'Reto Poas', Fecha: '11/11/2022', Recorrido: 'Poas', Tipo: 'Caminata'},
+  {Nombre: 'Aguas abiertas', Fecha: '11/11/2022', Recorrido: 'Herradura', Tipo: 'Natacion'},
+  {Nombre: 'Valle de Orosi', Fecha: '11/11/2022', Recorrido: 'Orosi', Tipo: 'Ciclismo'},
+  {Nombre: 'Jaco Extremo', Fecha: '11/11/2022', Recorrido: 'Jaco', Tipo: 'Carrera'},
+];
+
 @Component({
   selector: 'app-reportes-participantes',
   templateUrl: './reportes-participantes.component.html',
   styleUrls: ['./reportes-participantes.component.css']
 })
-export class ReportesParticipantesComponent implements OnInit {
 
+
+export class ReportesParticipantesComponent implements OnInit {
+  displayedColumns: string[] = ['Nombre', 'Fecha', 'Recorrido', 'Tipo', 'acciones'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
+
+ 
